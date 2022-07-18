ddbe8eec5de7d340a2e3ec25c288b1903ab5d9a5
leandrets <leleponsano@gmail.com>
2022-07-18 15:11:23 -0300

    Extrator refeito, dessa vez utilizando o OpenCV

--- extractor.py
+++ extractor.py
@@ -1,11 +1,8 @@
 import av
 import ast
-
-class Extractor:
+import cv2 as cv
     
-    containers = {}
-    streams = {}
-    frameSets = {}
+class Extractor:
     n = 0
     
     def readMessage(self, message):
@@ -19,25 +16,31 @@ class Extractor:
     def extract(self, message):
         ref, time, op = self.readMessage(message)
         
-        if ref not in self.containers:
-            self.containers[ref] = av.open(ref)
-            self.streams[ref] = self.containers[ref].streams.video[0]
-            self.frameSets[ref] = self.containers[ref].decode(self.streams[ref])
-        
-        frame_rate = int(self.streams[ref].average_rate)
-        index = self.calculateIndex(time, frame_rate)
-        
-        sucess = False
-        for idx, frame in enumerate(self.frameSets[ref]):
-            if abs(idx-index) < 1:
-                frame.to_image().save("ref/frames/frame%d.jpg" % self.n)
-                print(f"{self.n}: Successfully extracted frame {index} of video {ref}")
-                sucess = True
-                break
-        if not sucess:
-            print(f"{self.n}: Failed to extract frame {index} of video {ref}")
+        videoCap = cv.VideoCapture(ref)
+        if not videoCap.isOpened():
+            print(f"{self.n}: Couldnt open video {ref}")
+            
+        time = self.calculatePosition(time, videoCap)
+        success = videoCap.set(cv.CAP_PROP_POS_MSEC, time*1000)
+        if not success:
+            print(f"{self.n}: Couldnt set position {time}s  in video {ref}")  
+    
+        success, image = videoCap.read()
+        if success:
+            cv.imwrite("ref/frames/frame%d.jpg" %self.n, image)
+            print(f"{self.n}: Successfully extracted frame {time}s  in video {ref}")
+        else:
+            print(f"{self.n}: Couldnt read frame in {time}s from video {ref}")  
+
         self.n += 1
         
-    def calculateIndex(self, time, rate):
-        return (time * rate) - 1
-    
+    def calculatePosition(self, time, videoCap):
+        n_frames = videoCap.get(cv.CAP_PROP_FRAME_COUNT)
+        videoCap.set(cv.CAP_PROP_POS_FRAMES, n_frames)
+        duration = videoCap.get(cv.CAP_PROP_POS_MSEC)
+        if time > duration:
+            print(f"{self.n}: Time required is bigger than video duration")
+            time = duration
+        videoCap.set(cv.CAP_PROP_POS_FRAMES, 0)
+        return time
+          
